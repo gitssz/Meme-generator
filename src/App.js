f144d210feefc980b51a1e3d882eff02236dedbd
Geetanjali <83583240+gitssz@users.noreply.github.com>
2023-08-14 10:32:50 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header  from './components/Header';
+import Form from "./components/Form";
+function App() {
+  return (
+    <div className="app">
+     <Header />
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
