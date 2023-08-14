03b8d03b803462915013d3a21bc94d2ba43c46b5
Geetanjali <83583240+gitssz@users.noreply.github.com>
2023-08-14 10:57:24 +0530

    after adding image

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "../App.css";
+import cat from "../images/cat.jpeg"
 
 function Form() {
   return (
     <div className="main">
       <div className="form-container">
         <div className="form--input-container">
-          <input type="text" className="form--input" placeholder="Shut up"></input>
-          <input type="text" className="form--input" placeholder="take my"></input>
+          <input type="text" className="form--input" placeholder="top text"></input>
+          <input type="text" className="form--input" placeholder="bottom text"></input>
         </div>
         <button type="submit" className="btn">
           Get a new meme image 🖼
         </button>
-      </div>
-      <img src="" alt="meme" />
+      <div className="img">
+      <img src={cat} alt="meme" />
+    </div>
+    </div>
+
     </div>
   );
 }
