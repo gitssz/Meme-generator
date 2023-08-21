f8eac263e11b29f95774e69412ec7b6d7af9bd23
Geetanjali <83583240+gitssz@users.noreply.github.com>
2023-08-21 10:12:57 +0530

    Add files via upload

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "../App.css";
 import cat from "../images/cat.jpeg"
+import Data from "../meme_generator_data";
 
-function Form() {
+function Form(props) {
+function getImage(){
+  // console.log(Data.data.memes[parseInt(Math.random()*100)].url);
+  const memeArrays=Data.data.memes;
+  const randomNumber=Math.floor(Math.random()* memeArrays.length);
+  console.log(memeArrays[randomNumber].url);
+}
+
   return (
     <div className="main">
       <div className="form-container">
-        <div className="form--input-container">
-          <input type="text" className="form--input" placeholder="top text"></input>
-          <input type="text" className="form--input" placeholder="bottom text"></input>
-        </div>
-        <button type="submit" className="btn">
+          <div className="form--input-container">
+              <input type="text" className="form--input" placeholder="top text"></input>
+              <input type="text" className="form--input" placeholder="bottom text"></input>
+          </div>
+          <button type="submit" className="btn" onClick={getImage}>
           Get a new meme image 🖼
-        </button>
+          </button>
       <div className="img">
       <img src={cat} alt="meme" />
     </div>
